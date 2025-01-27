9a6332a1abba21558d3bc6973e6e8fd4fc47ae75
AliTafni <atafni53@gmail.com>
2025-01-27 12:21:00 +0100

    6560886

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,7 @@ const Card = ({ title, description }: CardProps) => {
    
       <div
         className="p-6 bg-white bg-opacity-5 rounded-xl shadow-md hover:shadow-lg transition-all cursor-pointer border border-red-200"
-        onClick={() => setIsOpen(true)}
+       
       >
         <h3 className="text-2xl font-bold text-gray-800 mb-3">{title}</h3>
         <p className="text-gray-600">{description}</p>
