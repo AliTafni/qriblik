4fe822c64c065b8b227d8dab9d49fb11475b1073
AliTafni <atafni53@gmail.com>
2025-01-27 12:17:53 +0100

    656856

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
+
 
 type CardProps = {
   title: string;
@@ -7,17 +6,19 @@ type CardProps = {
   dialogContent: string;
 };
 
-const Card = ({ title, description, dialogContent }: CardProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+const Card = ({ title, description }: CardProps) => {
 
   return (
-    <div
-      className="p-6 bg-white bg-opacity-25 rounded-xl shadow-md hover:shadow-lg transition-all cursor-pointer border border-red-200"
-      onClick={() => setIsOpen(true)}
-    >
-      <h3 className="text-2xl font-bold text-gray-800 mb-3">{title}</h3>
-      <p className="text-gray-600">{description}</p>
-    </div>
+   
+      <div
+        className="p-6 bg-white bg-opacity-5 rounded-xl shadow-md hover:shadow-lg transition-all cursor-pointer border border-red-200"
+        onClick={() => setIsOpen(true)}
+      >
+        <h3 className="text-2xl font-bold text-gray-800 mb-3">{title}</h3>
+        <p className="text-gray-600">{description}</p>
+      </div>
+
+      
   );
 };
 
